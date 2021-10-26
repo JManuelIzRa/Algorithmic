5c3ebe24d8e19f77fb695fd0c56050183a9b69c2
JManuelIzRa <jmanuel.izquierdo00@gmail.com>
2021-10-26 08:19:24 -0700

    makefile: in progress

---
+++ P3/funciones.h
@@ -0,0 +1,10 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#include <vector>
+#include "divisa.hpp"
+
+void cargaDivisas(std::vector <Divisa> &divisas);
+bool sortFunction (Divisa i,Divisa j);
+
+#endif
