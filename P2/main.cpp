32e12aca90db6256c2f4d86fb4873d78cc50329e
JManuelIzRa <jmanuel.izquierdo00@gmail.com>
2021-10-26 07:21:24 -0700

    main: DONE

--- P2/main.cpp
+++ P2/main.cpp
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include "funciones.h"
+#include "quicksort.h"
+#include "fusion.h"
 
 using namespace std;
 
@@ -11,38 +13,43 @@ int main(int argc, char** argv)
 
     srand(time(NULL));
 
-    cout<<"-------------------"<<endl;
-    cout<<"Elija una opcion:"<<endl;
-    cout<<"1. Ordenar con quicksort."<<endl;
-    cout<<"2. Ordenar por fusion."<<endl;
-    cout<<"3. Salir."<<endl;
-    cout<<"-------------------"<<endl;
-    cin>>option;
-
-    while(option<1 || option>3)
+    while(option != 3)
     {
-        cout<<"Error. Seleccione una de las opciones:"<<endl;
+        cout<<"-------------------"<<endl;
         cout<<"Elija una opcion:"<<endl;
-        cout<<"1. Ordenar con quicksort"<<endl;
-        cout<<"2. Ordenar por fusion"<<endl;
+        cout<<"1. Ordenar con quicksort."<<endl;
+        cout<<"2. Ordenar por fusion."<<endl;
         cout<<"3. Salir."<<endl;
+        cout<<"-------------------"<<endl;
         cin>>option;
 
-    }
+        while(option<1 || option>3)
+        {
+            cout<<"Error. Seleccione una de las opciones:"<<endl;
+            cout<<"Elija una opcion:"<<endl;
+            cout<<"1. Ordenar con quicksort"<<endl;
+            cout<<"2. Ordenar por fusion"<<endl;
+            cout<<"3. Salir."<<endl;
+            cin>>option;
 
-    switch(option)
-    {
-        case 1:
-            ordenacionQuickSort();
-            break;
+        }
 
-        case 2:
-            //ordenacionFusion();
-            break;
+        switch(option)
+        {
+            case 1:
+                ordenacionQuickSort();
+                system("./generarGrafico.sh && xdg-open graficoComparativo.eps");
+                break;
+
+            case 2:
+                ordenacionFusion();
+                system("./generarGrafico.sh && xdg-open graficoComparativo.eps");
+                break;
         
-        case 3:
-            std::cout << "Ha elegido salir" << std::endl;
-            break;
+            case 3:
+                std::cout << "Ha elegido salir" << std::endl;
+                break;
+        }
     }
 
     return 0;
